1c1eb9d71bcfd2947a70b8aed908ae89223a2240
Sindre Eiklid <sindre0830@gmail.com>
2024-07-24 14:39:37 +0200

    Update player movement and camera position

--- Assets/Script/FPSController.cs
+++ Assets/Script/FPSController.cs
@@ -5,13 +5,11 @@ namespace Script {
     public class FPSController : MonoBehaviour {
         public Camera playerCamera;
         public float walkSpeed = 6.0f;
-        public float runSpeed = 12.0f;
-        public float gravity = 10.0f;
         private Vector3 _moveDirection = Vector3.zero;
         private float _rotationX;
         public bool canMove = true;
         private CharacterController _characterController;
-        private Vector3 _intialCameraPosition;
+        private Vector3 _initialCameraPosition;
         
         
         // Start is called before the first frame update
@@ -19,50 +17,46 @@ namespace Script {
             _characterController = GetComponent<CharacterController>();
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
-            _intialCameraPosition = playerCamera.transform.position;
+            _initialCameraPosition = playerCamera.transform.position;
         }
 
         // Update is called once per frame
         private void Update() {
-            #region Handles Movement
+            Move();
+
+            Rotation();
+
+            UpdateCameraPosition();
+        }
+
+        private void Move() {
+            Vector3 forward = playerCamera.transform.TransformDirection(Vector3.forward);
+            Vector3 right = playerCamera.transform.TransformDirection(Vector3.right);
+
+            float curSpeedX = canMove ? walkSpeed * Input.GetAxis("Vertical") : 0;
+            float curSpeedY = canMove ? walkSpeed * Input.GetAxis("Horizontal") : 0;
             
-            Vector3 forward = Vector3.forward;
-            Vector3 right = Vector3.right;
+            _moveDirection = (forward * curSpeedX + right * curSpeedY).normalized;
 
-            bool isRunning = Input.GetKey(KeyCode.LeftShift);
-            float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-            float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
-            
-            _moveDirection = forward * curSpeedX + right * curSpeedY;
-            
-            #endregion
-
-            #region Handles Jumping
-
-            if (!_characterController.isGrounded) {
-                _moveDirection.y -= gravity * Time.deltaTime;
-            }
-
-            #endregion
-
-            #region Handles Rotation
+            _moveDirection *= canMove ? walkSpeed : 0;
+            _moveDirection.y = 0.0f;
 
             _characterController.Move(_moveDirection * Time.deltaTime);
+        }
 
-            if (!canMove) return;
-
+        private void Rotation() {
             Ray cameraRay = playerCamera.ScreenPointToRay(Input.mousePosition);
             Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
 
-            if (groundPlane.Raycast(cameraRay, out float rayLength)) {
-                Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-
-                transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
-            }
-
-            #endregion
+            if (!groundPlane.Raycast(cameraRay, out float rayLength)) return;
             
-            playerCamera.transform.position = _intialCameraPosition + transform.position;
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+
+        private void UpdateCameraPosition() {
+            playerCamera.transform.position = _initialCameraPosition + transform.position;
         }
     }
 }
