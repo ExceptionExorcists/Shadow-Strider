4e99470a0e7d582de17d7fa5198e927da5f48ccb
janstrng <janolafstoreng@gmail.com>
2024-07-30 19:26:15 +0200

    fixed text outline

--- Assets/Script/documentScript.cs
+++ Assets/Script/documentScript.cs
@@ -8,11 +8,13 @@ public class documentScript : MonoBehaviour
     private GameObject player;
     public float disableTextDistance;
     public TMP_Text textfield;
+    public Material[] textMaterial;
     // Start is called before the first frame update
     void Start()
     {
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         player = players[0];
+        textfield.gameObject.GetComponent<MeshRenderer>().materials = textMaterial;
     }
 
     // Update is called once per frame
